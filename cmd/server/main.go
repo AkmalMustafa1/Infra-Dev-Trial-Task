ae28f240d059f433adff20058c41acd76cc09669
Akmal Mustafa <akmalperhyar@gmail.com>
2025-08-31 22:18:42 +0100

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,31 @@ import (
     "net/http"
     "os"
 
-    "Infra-Dev-Trial-Task/internal/handlers"
+    "package main
+
+import (
+    "log"
+    "net/http"
+    "os"
+
+    "github.com/AkmalMustafa1/Infra-Dev-Trial-Task/internal/handlers"
+)
+
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    mux := http.NewServeMux()
+    mux.HandleFunc("/api/get-balance", handlers.GetBalanceHandler)
+
+    log.Printf("Server listening on :%s", port)
+    if err := http.ListenAndServe(":"+port, mux); err != nil {
+        log.Fatal(err)
+    }
+}
+"
 )
 
 func main() {
